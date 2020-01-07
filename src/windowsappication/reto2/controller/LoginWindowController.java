e384ed8c4d1d88d8914ac1f6fe44567ebcbe6a8a
2dam <2dam@MACINF07.TartangaLH.eus>
2020-01-07 08:47:36 +0100

    Esqueleto de los controladores

---
+++ src/windowsappication/reto2/controller/LoginWindowController.java
@@ -0,0 +1,290 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windowsappication.reto2.controller;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import utilities.exception.LoginNotFoundException;
+import utilities.exception.UserPasswordNotFoundException;
+
+
+/**
+ * This class is a controller UI class for LogIn_Window view. Contains event
+ * handlers and on window showing code.
+ *
+ * @author Adrian Corral
+ */
+public class LoginWindowController {
+
+    /**
+     * The stage is used for instance an stage attribute and can store the stage
+     * from other class or send the stage to other class.
+     */
+    private Stage stage;
+
+    /**
+     * This is the login button on the view.
+     */
+    @FXML
+    private Button btLogin;
+
+    /**
+     * This is the text input by the user that define the username.
+     */
+    @FXML
+    private TextField txtLogin;
+
+    /**
+     * This is the text input by the user that define the password.
+     */
+    @FXML
+    private PasswordField txtPass;
+
+    /**
+     * This is the link to navigate to the sign up window.
+     */
+    @FXML
+    private Hyperlink linkClickHere;
+
+    @FXML
+    private Label lbLogin;
+
+    @FXML
+    private Label lbPass;
+
+    private static final Logger LOGGER = Logger.getLogger(
+        "WindowsClientApplication.controller.LoginWindowController");
+
+    /**
+     * @return Return the stage of this class
+     */
+    public Stage getStage() {
+        return stage;
+    }
+
+    /**
+     * @param stage Sets the stage for this class
+     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    /**
+     * This method initialize the window and everything thats the stage needs.
+     * This calls other method when shows the window to set attributes of the
+     * window
+     *
+     * @param root The parent object
+     */
+    public void initStage(Parent root) {
+        Scene scene = new Scene(root);
+        //Stage Properties
+        stage.setScene(scene);
+        stage.setTitle("LogIn");
+        stage.setResizable(false);
+        stage.setOnShowing(this::handleWindowShowing);
+        stage.setOnCloseRequest(this::closeRequest);
+
+        //Listeners
+        txtLogin.textProperty().addListener(this::textChange);
+        txtPass.textProperty().addListener(this::textChange);
+
+        //Stage show
+        stage.show();
+    }
+
+    /**
+     * This is the method to control the components of this window when we shows
+     * the window.
+     *
+     * @param event The event is the window that is being showed.
+     */
+    private void handleWindowShowing(WindowEvent event) {
+        btLogin.setDisable(true);
+        btLogin.setMnemonicParsing(true);
+        btLogin.setText("_Login");
+        btLogin.setTooltip(new Tooltip("Click to complete the login"));
+        lbLogin.setTooltip(new Tooltip("Username to login"));
+        lbPass.setTooltip(new Tooltip("Password to login"));
+        linkClickHere.setTooltip(new Tooltip("Click here to sign up"));
+    }
+
+    /**
+     * This method is used if the user try to close the application clicking in
+     * the red cross(right-top in the stage) and control if the user is sure to
+     * close the application.
+     *
+     * @param event The event is the user trying to close the application with
+     * the cross of the stage.
+     */
+    public void closeRequest(WindowEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Close confirmation");
+        alert.setTitle("Exit Window");
+        alert.setContentText("Are you sure that want close the application?");
+        alert.initOwner(stage);
+        alert.initModality(Modality.WINDOW_MODAL);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            stage.close();
+            Platform.exit();
+
+        } else {
+            event.consume();
+        }
+    }
+
+    /**
+     * Checks every time that the user change the TextField and if both formats
+     * are correct enables the login button.
+     *
+     * @param event The event when the text is changing.
+     */
+    private void textChange(ObservableValue observable, String oldValue, String newValue) {
+        boolean passok = checkPassword(txtPass.getText().trim());
+        boolean usernameOk = false;
+        boolean passwordCheck = false;
+        //Check if user got the correct format
+        if (txtLogin.getText().trim().length() >= 4
+            && txtLogin.getText().trim().length() <= 10) {
+            //Check if password got the correct format
+            usernameOk = true;
+
+        } else {
+
+            usernameOk = false;
+
+        }
+        if (txtPass.getText().trim().length() >= 8
+            && txtPass.getText().trim().length() <= 14 && passok) {
+            //Enables LogIn button
+            passwordCheck = true;
+
+        } else {
+            passwordCheck = false;
+        }
+        if (usernameOk && passwordCheck) {
+            btLogin.setDisable(false);
+        } else {
+            btLogin.setDisable(true);
+        }
+    }
+
+    /**
+     * This method checks if the password has the correct format.
+     *
+     * @param password a String that contains the passwors written by the user.
+     * @return check A boolean.
+     */
+    private boolean checkPassword(String password) {
+
+        boolean capital = false;
+        boolean number = false;
+        boolean check = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)) {
+                number = true;
+            }
+            if (Character.isUpperCase(ch)) {
+                capital = true;
+            }
+        }
+
+        if (capital && number) {
+            check = true;
+        }
+        return check;
+    }
+
+    /**
+     * This method sends the txtLogin and the txtPass to the factory and waits
+     * if the user exists on dataBase and the password is correct to go to the
+     * logout window.
+     *
+     * @param event The event is the user clicking on the login button.
+     * @throws LoginNotFoundException If login does not exist in the database.
+     * @throws WrongPasswordException If password does not match with the user.
+     */
+    
+    
+    public void loginClick(ActionEvent event) throws LoginNotFoundException, UserPasswordNotFoundException {
+        /*try {
+            
+        } catch (LoginNotFoundException e) {
+            LOGGER.warning("LoginWindowController: Login not found");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("LogIn Error");
+            alert.setContentText("User does not exist");
+            alert.showAndWait();
+        } catch (UserPasswordNotFoundException e) {
+            LOGGER.warning("LoginWindowController: Wrong password");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Password Error");
+            alert.setContentText("Password does not exist");
+            alert.showAndWait();
+        } catch (ServerConnectionErrorException ex) {
+            LOGGER.warning("LoginWindowController: Server connection error");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Server Error");
+            alert.setContentText("Unable to connect with server");
+            alert.showAndWait();
+        } catch (IOException ex) {
+            LOGGER.warning("LoginWindowController: IO Exception on LoginWindowController");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("Sorry, an error has ocurred");
+            alert.showAndWait();
+        } catch (Exception ex) {
+            LOGGER.warning("LoginWindowController: Exception on LoginWindowController");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("Sorry, an error has ocurred");
+            alert.showAndWait();
+        }*/
+    }
+
+    /**
+     * This method opens the sign up window when the user clicks on the
+     * hyperlink click here
+     *
+     * @param event The event
+     * @throws IOException Error when can't access to the fxml view
+     */
+    public void signUpClick(ActionEvent event) throws IOException {
+        //Clean Login windows Login and Password fields before loading the Sign 
+        //up window.
+        txtLogin.setText("");
+        txtPass.setText("");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(
+            "/windowsclientapplication/view/SignUp_Window.fxml"));
+        Parent root = (Parent) loader.load();
+        SignUpWindowController signUpController
+            = ((SignUpWindowController) loader.getController());
+        signUpController.setStage(stage);
+        signUpController.initStage(root);
+    }
+
+}
---
+++ src/windowsappication/reto2/controller/SignUpWindowController.java
@@ -0,0 +1,472 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windowsappication.reto2.controller;
+
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Paint;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import utilities.util.Util;
+
+
+/**
+ * This class is a controller UI class for SignUp_Window view. Contains event
+ * handlers and on window showing code.
+ *
+ * @author Aimar Arrizabalaga and Gaizka Andrés
+ */
+public class SignUpWindowController {
+
+    private static final Logger LOGGER = Logger
+        .getLogger("WindowsClientApplication.controller.SignUpWindowController");
+
+    @FXML
+    private Button btBack;
+    @FXML
+    private Button btSignUp;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtRepeatPassword;
+    @FXML
+    private TextField txtFullName;
+    @FXML
+    private Label lbUsernameCaution;
+    @FXML
+    private Label lbEmailCaution;
+    @FXML
+    private Label lbPasswordCaution1;
+    @FXML
+    private Label lbPasswordCaution2A;
+    @FXML
+    private Label lbPasswordCaution2B;
+    @FXML
+    private Label lbPasswordCaution3;
+    @FXML
+    private Label lbFullNameCaution;
+    @FXML
+    private Label lbUsername;
+    @FXML
+    private Label lbPassword;
+    @FXML
+    private Label lbEmail;
+    @FXML
+    private Label lbRepeatPassword;
+    @FXML
+    private Label lbFullName;
+    @FXML
+    private Button btHelp;
+
+    private Stage stage;
+
+    /**
+     * Constructor that gets the stage from login
+     *
+     * @param stage the stage for this class
+     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    /**
+     * This method initialize the window and everything thats the stage needs.
+     * This calls other method when shows the window to set attributes of the
+     * window
+     *
+     * @param root The parent object
+     */
+    public void initStage(Parent root) {
+        Scene scene = new Scene(root);
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Registration");
+        stage.setResizable(false);
+        stage.setOnShowing(this::handleWindowShowing);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        btBack.setOnAction(this::handleButtonAction);
+        btSignUp.setOnAction(this::handleButtonAction);
+        btHelp.setOnAction(this::helpButtonAction);
+        stage.setOnCloseRequest(this::handleCloseAction);
+        btSignUp.setDisable(false);
+
+       
+        stage.getScene().addEventFilter(KeyEvent.KEY_PRESSED, this::helpshortcut);
+
+        stage.show();
+    }
+
+    /**
+     * This is the method to control the components of this window when we shows
+     * the window.
+     *
+     * @param event The event is the window that is being showed.
+     */
+    private void handleWindowShowing(WindowEvent event) {
+        LOGGER.info("Setting the window...");
+        txtUsername.setPromptText("Introduce login");
+        txtEmail.setPromptText("Introduce email");
+        txtPassword.setPromptText("Introduce password");
+        txtRepeatPassword.setPromptText("Repeat password");
+        txtFullName.setPromptText("Introduce full name");
+        btSignUp.setTooltip(new Tooltip("Click to complete the registration"));
+        btBack.setTooltip(new Tooltip("Return to LogIn"));
+        lbUsername.setTooltip(new Tooltip("Username to login"));
+        lbPassword.setTooltip(new Tooltip("Password to login"));
+        lbRepeatPassword.setTooltip(new Tooltip("Repeat the password"));
+        lbEmail.setTooltip(new Tooltip("Email to send information"));
+        lbFullName.setTooltip(new Tooltip("Your Full Name"));
+        btSignUp.setMnemonicParsing(true);
+        btBack.setMnemonicParsing(true);
+        btSignUp.setText("_Sign Up");
+        btBack.setText("_Back");
+
+    }
+
+    
+    /**
+     * A method that registre the key pressed
+     *
+     * @param ke The event when press a key
+     */
+    public void helpshortcut(KeyEvent ke) {
+        KeyCode pressButton = ke.getCode();
+        if (pressButton.equals(KeyCode.F1)) {
+            helpAction();
+        }
+    }
+
+    /**
+     * This method is used if the user try to close the application clicking in
+     * the red cross(right-top in the stage) and control if the user is sure to
+     * close the application.
+     *
+     * @param event The event is the user trying to close the application with
+     * the cross of the stage.
+     *
+     */
+    private void handleCloseAction(WindowEvent event) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Close confirmation");
+        alert.setHeaderText("You pressed the 'Close'. \n"
+            + "Registration will be cancelled.");
+        alert.setContentText("Are you sure?");
+        alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.YES) {
+            stage.close();
+        } else {
+            event.consume();
+        }
+    }
+
+   
+    /**
+     * A method that registres the help button
+     *
+     * @param event The event when the user click on the help button
+     *
+     */
+    
+    public void helpButtonAction(ActionEvent event) {
+        helpAction();
+    }
+
+    /**
+     * A method that open the help window
+     */
+    
+    public void helpAction() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().
+                getResource("/windowsclientapplication/view/SignUp_Help.fxml"));
+
+            Parent root = (Parent) loader.load();
+            HelpWindowController Helpcontroller = loader.getController();
+            Helpcontroller.setStage(stage);
+            Helpcontroller.initStage(root);
+        } catch (IOException ex) {
+            LOGGER.warning("SignUpWindowController: IO Exception on SignUpWindowController");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("An error has ocurred");
+            alert.showAndWait();
+        }
+    }
+    /**
+     * A method that registres the button actions
+     *
+     * @param event The event is the user clicking on the buttons.
+     */
+    public void handleButtonAction(ActionEvent event) {
+
+        if (event.getSource().equals(btBack)) {
+            LOGGER.info("Closing the window");
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Close confirmation");
+            alert.setHeaderText("You pressed the 'Close'. \n"
+                + "Registration will be cancelled.");
+            alert.setContentText("Are you sure?");
+            alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.YES) {
+                stage.close();
+            } else {
+                alert.close();
+            }
+        }
+        if (event.getSource().equals(btSignUp)) {
+            //try {
+                if (checkValidation()) {
+                    
+                    
+                    
+                    //An alert to let the user know the signing up's been correct.
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("User Sent");
+                    alert.setHeaderText("Registration completed.");
+                    Button okButton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK);
+                    okButton.setId("okbutton");
+                    Optional<ButtonType> result = alert.showAndWait();
+                    if (result.get() == ButtonType.OK) {
+
+                        stage.close();
+
+                    }
+                    LOGGER.info("User sent correctly");
+                } else {
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("ERROR");
+                    alert.setHeaderText("Sing up failed");
+                    alert.setContentText("Check the validation tips below");
+                    Button errorButton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK);
+                    errorButton.setId("errorbutton");
+                    Optional<ButtonType> result = alert.showAndWait();
+                    if (result.get() == ButtonType.YES) {
+                        alert.close();
+
+                    }
+                    LOGGER.info("Error sending user");
+                }
+            /*} catch (LoginAlreadyTakenException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("SignUp Error");
+                alert.setContentText("An error ocurred trying to sign up, "
+                + "login arleady taken.");
+                Button LoginTakenButton = (Button) alert.getDialogPane().lookupButton(ButtonType.OK);
+                LoginTakenButton.setId("loginTakenButton");
+                alert.showAndWait();
+                
+            } catch (ServerConnectionErrorException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("LogIn Error");
+                alert.setContentText("An error ocurred trying to sign up, "
+                    + "can not connect with the server.");
+                alert.showAndWait();
+            }
+            */  
+        }
+    }
+
+    /**
+     * A method that validates the pattern of the email
+     *
+     * @param email A string with the email
+     * @return check A boolean that return the checking of the email
+     */
+    
+    private boolean checkEmail(String email) {
+        boolean check = false;
+        check = Util.validarEmail(email);
+        return check;
+    }
+
+    /**
+     * A method that validates if both password fields are the same
+     *
+     * @param password the password the client set
+     * @param passwordRepeat the repetition of the password
+     * @return checkRepeat A boolean that return the check of the passwords is
+     * ok
+     */
+    private boolean checkPassRepeat(String password, String passwordRepeat) {
+        boolean checkRepeat = false;
+
+        if (password.equalsIgnoreCase(passwordRepeat)) {
+            checkRepeat = true;
+        }
+
+        return checkRepeat;
+    }
+
+    /**
+     * A method that checks if the password has an uppercase and a number
+     *
+     * @param password the password the client set
+     * @return check A boolean that return if the validation is ok
+     */
+    private boolean checkPassword(String password) {
+
+        boolean capital = false;
+        boolean number = false;
+        boolean check = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)) {
+                number = true;
+            }
+            if (Character.isUpperCase(ch)) {
+                capital = true;
+            }
+        }
+
+        if (capital && number) {
+            check = true;
+        }
+        return check;
+    }
+
+    /**
+     * A method that validate the text change of the fields
+     *
+     */
+    private boolean checkValidation() {
+        boolean passCheck = checkPassword(txtPassword.getText().trim());
+        boolean passCheckRepeat = checkPassRepeat(txtPassword.getText()
+            .trim(), txtRepeatPassword.getText().trim());
+        boolean emailCheck = checkEmail(txtEmail.getText().trim());
+
+        boolean username = false;
+        boolean passwordlength = false;
+        boolean passwordCheck = false;
+        boolean passwordRepeat = false;
+        boolean email = false;
+        boolean fullname = false;
+        boolean ok = false;
+
+        if (txtUsername.getText().trim().length() > 3
+            && txtUsername.getText().trim().length() < 11) {
+
+            username = true;
+            lbUsernameCaution.setTextFill(Paint.valueOf("BLACK"));
+
+        } else {
+
+            lbUsernameCaution.setTextFill(Paint.valueOf("RED"));
+          
+
+            txtUsername.requestFocus();
+        }
+
+        if (txtPassword.getText().trim().length() > 7
+            && txtPassword.getText().trim().length() < 15) {
+
+            passwordlength = true;
+            lbPasswordCaution1.setTextFill(Paint.valueOf("BLACK"));
+
+        } else {
+
+            lbPasswordCaution1.setTextFill(Paint.valueOf("RED"));
+            
+            if (!txtUsername.isFocused() && !txtEmail.isFocused() && !txtEmail.isFocused()) {
+                txtPassword.requestFocus();
+            }
+
+        }
+
+        if (passCheck) {
+
+            passwordCheck = true;
+            lbPasswordCaution2A.setTextFill(Paint.valueOf("BLACK"));
+            lbPasswordCaution2B.setTextFill(Paint.valueOf("BLACK"));
+
+        } else {
+
+            lbPasswordCaution2A.setTextFill(Paint.valueOf("RED"));
+            lbPasswordCaution2B.setTextFill(Paint.valueOf("RED"));
+            
+            if (!txtUsername.isFocused() && !txtEmail.isFocused() && !txtEmail.isFocused()) {
+                txtPassword.requestFocus();
+            }
+        }
+
+        if (passCheckRepeat) {
+
+            passwordRepeat = true;
+            lbPasswordCaution3.setTextFill(Paint.valueOf("BLACK"));
+        } else {
+
+            lbPasswordCaution3.setTextFill(Paint.valueOf("RED"));
+            
+            if (!txtUsername.isFocused() && !txtEmail.isFocused() && !txtEmail.isFocused() && !txtPassword.isFocused()) {
+                txtRepeatPassword.requestFocus();
+            }
+
+        }
+        if (emailCheck) {
+
+            email = true;
+            lbEmailCaution.setTextFill(Paint.valueOf("BLACK"));
+        
+        } else {
+
+            lbEmailCaution.setTextFill(Paint.valueOf("RED"));
+            
+            if (!txtUsername.isFocused()) {
+                txtEmail.requestFocus();
+            }
+        
+        }
+        
+
+        if (!txtFullName.getText().trim().isEmpty() && txtFullName.getText().trim().length() < 44) {
+
+            fullname = true;
+            lbFullNameCaution.setTextFill(Paint.valueOf("BLACK"));
+
+        } else {
+
+            lbFullNameCaution.setTextFill(Paint.valueOf("RED"));
+ 
+            if (!txtUsername.isFocused() && !txtEmail.isFocused()) {
+                txtFullName.requestFocus();
+            }
+
+        }
+
+        if (username && passwordlength && passwordRepeat && passwordCheck
+            && email && fullname) {
+
+            ok = true;
+
+        }
+
+        return ok;
+    }
+
+}
