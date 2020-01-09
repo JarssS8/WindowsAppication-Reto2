6b5b053a6e03b060bdf0ee8f2bc870734493145e
2dam <2dam@MACINF03.TartangaLH.eus>
2020-01-09 13:00:55 +0100

    Group controllers 09/01/2020 V3

--- src/windowsapplication/controller/InfoGroupWindowController.java
+++ src/windowsapplication/controller/InfoGroupWindowController.java
@@ -3,12 +3,151 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package windowsapplication.controller;
+package windowsappication.controller;
+
+import java.util.Optional;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
- *
- * @author Gaizka Andres
+ * This class is a controller of the "VerGrupos" view. Contains event
+ * handlers and on window showing code.
+ * @author Diego Urraca
  */
 public class InfoGroupWindowController {
+    private static final Logger LOGGER = Logger
+            .getLogger("windowsclientapplication.LogOutWindowController");
+    /**
+     * Combo box with the names of the groups
+     */
+    @FXML
+    private ComboBox conGroups;
+     /**
+     * Label for the name of the admin of the group
+     */
+    @FXML
+    private Label lbGroup;
+    /**
+     * Button to search for a group
+     */
+    @FXML
+    private Button btSearch;
+    /**
+     * Button to go back
+     */
+    @FXML
+    private Button btBack;
     
+    /**
+     * The stage of this window
+     */
+    private Stage stage;
+    /**
+     * Object user to fillwith the data of the user that's loging in
+     */
+    private User user = null;
+    
+    
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    
+    
+    
+    /**NOTAS
+     * 
+     * Al iniciar,cargar una array con los grupos y su datos del usuario, en el
+     * combobox meter los nombres, y cuandode al boton de buscar, se le envia al metodo el grupos y su datos
+     * 
+     */
+    
+    
+    
+    public void initStage(Parent root, User user) {
+        try{
+            Scene scene = new Scene(root);
+            stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.setTitle("Search / Create Groups");
+            stage.setResizable(false);
+            stage.setOnShowing(this::onWindowShowing);
+            stage.setOnCloseRequest(this::handleCrossCloseAction);
+            btBack.setOnAction(this::handleBackAction);
+            btSearch.setOnAction(this::handleSearchGroupAction);
+            btCreateGroup.setOnAction(this::handleCreateGroupAction);
+            stage.show();
+        } catch (Exception e) {
+            LOGGER.severe("Can not initialize the group search/create window");
+        }
+        
+    }
+    public void handleCrossCloseAction(WindowEvent event) {
+        close();
+    }
+    
+    public void handleBackAction(){
+        stage.close();
+    }
+    
+    public void handleSearchGroupAction(){
+        
+    }
+    
+    public void handleCreateGroupAction(){
+        
+    }
+    /**
+     * Method that handle the close alert
+     */
+    public void close(){ //TODO adaptar a la nueva app
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close confirmation");
+        alert.setHeaderText("You are about to close the application.");
+        alert.setContentText("Are you sure?");
+        alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+        Button buttonYes = (Button) alert.getDialogPane().lookupButton(ButtonType.YES);
+        buttonYes.setId("buttonYes");
+        Button buttonNo = (Button) alert.getDialogPane().lookupButton(ButtonType.NO);
+        buttonNo.setId("buttonNo");
+        Optional<ButtonType> result = alert.showAndWait();
+        
+        //TODO actualizar fecha de conexion y cerrar programa
+        
+        if (result.get() == ButtonType.YES) {
+            try {
+                //client.logOut(user);
+                LOGGER.info("User loggin date updated sucesfully");
+            } /*catch (ServerConnectionErrorException e) {
+                LOGGER.warning("Couldn't connect to server. LogOut date not updated.");
+            }*/finally{
+                Platform.exit();
+            }
+        } else {
+            alert.close();
+        }
+        
+        
+    }
+    
+    /**
+     * Method that loads the texts and prepare the objects of the window
+     *
+     * @param event The event is the window that is being showed.
+     */
+    public void onWindowShowing(WindowEvent event) {//TODO adaptar a la nueva app
+        LOGGER.info("Starting loading the window");
+        
+    }
 }
--- src/windowsapplication/controller/SearchCreateGroupWindowController.java
+++ src/windowsapplication/controller/SearchCreateGroupWindowController.java
@@ -3,12 +3,196 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package windowsapplication.controller;
+package windowsappication.controller;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import windowsappication.service.DocumentClientREST;
 
 /**
- *
- * @author Gaizka Andres
+ * This class is a controller of the "BuscarCrearGrupos" view. Contains event
+ * handlers and on window showing code.
+ * @author Diego Urraca
  */
 public class SearchCreateGroupWindowController {
+    private static final Logger LOGGER = Logger
+            .getLogger("windowsclientapplication.LogOutWindowController");
+    /**
+     * Group name text input to search
+     */
+    @FXML
+    private TextField txtGroupName;
+    /**
+     * Group name text input to create
+     */
+    @FXML
+    private TextField txtNewGroup;
+    /**
+     * Password text input to create
+     */
+    @FXML
+    private TextField txtPassword;
+    /**
+     * Repeat password text input to ceate
+     */
+    @FXML
+    private TextField txtRepeatPassword;
+    /**
+     * Button to search for groups
+     */
+    @FXML
+    private Button btSearch;
+    /**
+     * Button to go back
+     */
+    @FXML
+    private Button btBack;
+    /**
+     * Button to create the group
+     */
+    @FXML
+    private Button btCreateGroup;
     
+    /**
+     * The stage of this window
+     */
+    private Stage stage;
+    /**
+     * Object user to fill with the data of the user that's loging in
+     */
+    private User user = null;
+    /**
+     * Object group to fill with the data of a group
+     */
+    private Group group = null;
+    /**
+     * Collection of groups
+     */
+    private Set <Group> groups= null;
+    
+    /**
+     * Method that set the stage for this window
+     * @param stage from Log in window
+     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    
+    
+    
+    
+    
+    /* NOTAS
+    
+    Si un usuario busca un grupo,debe poner su nombre exactamente.
+    Si se encuentra dicho grupo, saldra un ALERT que le preguntara la clave para unirse,puede cancelarse
+    si ya esta dentro del grupo, el ALERT le preguntara si quiere abandonar el grupo
+    Para todo esto, necesitamos comprobar la lista de grupos del usuario, y la lista de grupos con sus nombres y sus passwords
+    
+    
+    */
+    
+    
+    
+    
+    
+    
+    /**
+     * Method to initialize the window
+     * @param root the loader for the scene
+     * @param user The user wich is loged in 
+     */
+    public void initStage(Parent root, User user) {
+        try{
+            this.user = user;
+            Scene scene = new Scene(root);
+            stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.setTitle("Search / Create Groups");
+            stage.setResizable(false);
+            stage.setOnCloseRequest(this::handleCloseAction);
+            btBack.setOnAction(this::handleCloseAction);
+            btSearch.setOnAction(this::handleSearchGroupAction);
+            btCreateGroup.setOnAction(this::handleCreateGroupAction);
+            stage.show();
+        } catch (Exception e) {
+            LOGGER.severe("Can not initialize the group search/create window");
+        }
+        
+    }
+    
+    /**
+     * 
+     * @param event The user clic the cross to close the window
+     */
+    public void handleCloseAction(WindowEvent event) {
+        close();
+    }
+    
+    public void handleSearchGroupAction(){
+        this.group = null;
+        String groupName = txtGroupName.getText();
+        this.group = searchGroupByName(groupName);
+        if(null!=this.group)
+            joinGroup(this.group, this.user);
+    }
+    
+    public void handleCreateGroupAction(){
+        createGroup();
+    }
+
+    public void searchGroupByName(String groupName){
+        
+    }
+    
+    public void createGroup(){
+        
+    }
+    
+    public void joinGroup(Group group, User user){
+        try{
+            DocumentClientREST.joinGroup(group.groupname,group.password,user);
+        } catch(Exception e){
+            LOGGER.severe("Error joining the group");
+        }
+    }
+    
+    /**
+     * Method that handle the close alert
+     */
+    public void close(){
+        if(txtGroupName.getLength()!=0 || txtNewGroup.getLength()!=0 || txtPassword.getLength()!=0 || txtRepeatPassword.getLength()!=0){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Close confirmation");
+            alert.setHeaderText("You are about to close the window with some data.");
+            alert.setContentText("Are you sure?");
+            alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+            Button buttonYes = (Button) alert.getDialogPane().lookupButton(ButtonType.YES);
+            buttonYes.setId("buttonYes");
+            Button buttonNo = (Button) alert.getDialogPane().lookupButton(ButtonType.NO);
+            buttonNo.setId("buttonNo");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.YES) {
+                stage.close();
+            } else
+                alert.close();
+        }
+        else
+            stage.close();
+            
+    }
 }
